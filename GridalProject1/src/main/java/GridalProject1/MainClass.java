d8176e941fbd382248e2254174b279a6373f0981
Prabeshlama0@gmail.com <prabeshlama0@gmail.com>
2021-12-06 12:58:03 -0600

    Commiting

--- GridalProject1/src/main/java/GridalProject1/MainClass.java
+++ GridalProject1/src/main/java/GridalProject1/MainClass.java
@@ -5,11 +5,8 @@ public class MainClass {
 	public static void main (String [] args) {
 		System.out.println("Hello World");
 		
-		System.out.println("Hello World");
-		System.out.println("Hello World");
-		System.out.println("Hello World");
-		
-		System.out.println("Hello World");
+	
+
 		
 	}
 
